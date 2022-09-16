b4133352407c6bfb59680c08d59799a926b3f539
Eugene <104440043+erascu@users.noreply.github.com>
2022-09-16 17:41:15 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const menuLinks = document.querySelectorAll('.header__link[data-goto], .header__link_img[data-goto]')
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    })
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto)
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            })
+            e.preventDefault()
+        }
+    }
+}
