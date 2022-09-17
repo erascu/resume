15e4933613f650823f401bf3b4da8f94b157b953
Eugene <104440043+erascu@users.noreply.github.com>
2022-09-17 16:40:34 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-'use strict'
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Anroid/i)
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i)
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i)
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i) ||
+            navigator.userAgent.match(/WPDesktop/i)
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows())
+    }
+}
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch')
+
+} else {
+    document.body.classList.add('_pc')
+}
 
 const menuLinks = document.querySelectorAll('.header__link[data-goto], .header__link_img[data-goto]')
 
@@ -13,6 +47,12 @@ if (menuLinks.length > 0) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto)
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
 
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock')
+                iconMenu.classList.remove('_active')
+                menuBody.classList.remove('_active')
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: 'smooth'
@@ -20,4 +60,14 @@ if (menuLinks.length > 0) {
             e.preventDefault()
         }
     }
+}
+
+const iconMenu = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.header__nav')
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
 }
